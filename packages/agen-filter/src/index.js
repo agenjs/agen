f2d533e7b4d73962c9023a1b99a385ccd2e1e1bc
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2019-10-22 00:40:25 +0200

    feature: implement filters

---
+++ packages/agen-filter/src/index.js
@@ -0,0 +1,6 @@
+module.exports = async function* filter(provider, filter) {
+  let idx = 0;
+  for await (let value of provider) {
+    if (await filter(value, idx++)) yield value;
+  }
+}
---
+++ packages/agen-filter/test/index.js
@@ -0,0 +1,31 @@
+const expect = require('expect.js');
+const filter = require('../src');
+
+describe('filter', function() {
+
+  it(`should be able filter elements values`, async () => {
+    const list = [
+      'First Message',
+      'Hello world',
+      'Second Message',
+      'Hello John Smith'
+    ]
+    const control = ['Hello world', 'Hello John Smith'];
+    let idx = 0;
+    for await (let item of filter(list, (v) => v.indexOf('Hello') >= 0)) {
+      expect(item).to.eql(control[idx++]);
+    }
+    expect(idx).to.eql(control.length);
+  })
+
+  it(`should be able filter elements by their index (position)`, async () => {
+    const list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'];
+    const control = ['a', 'c', 'e', 'g', 'i', 'k'];
+    let idx = 0;
+    for await (let item of filter(list, (v, i) => i % 2 === 0)) {
+      expect(item).to.eql(control[idx++]);
+    }
+    expect(idx).to.eql(control.length);
+  })
+
+});
