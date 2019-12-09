1e40e9e5c816e3be6a463f72a78998efd4160b27
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2019-12-09 15:47:48 +0100

    feature: implement async generators providing buffers

---
+++ packages/agen-buffers/src/fixedSizeBuffers.js
@@ -0,0 +1,15 @@
+module.exports = async function* fixedSizeBuffers(provider, size) {
+  size = size || 1024;
+  let buf = Buffer.from([]);
+  for await (let chunk of provider) {
+    buf = Buffer.concat([buf, chunk]);
+    let start = 0;
+    while (buf.length - start >= size) {
+      const end = start + size;
+      yield buf.slice(start, end);
+      start = end;
+    }
+    if (start > 0) buf = buf.slice(start);
+  }
+  if (buf.length) yield buf;
+}
