0e100095491e6f520a74db01005fc913ba1d6c12
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2019-12-29 13:40:37 +0100

    refactor: reimplement the streamToPromise method

--- packages/agen-stream/rollup.config.js
+++ packages/agen-stream/rollup.config.js
@@ -1,8 +1,6 @@
 import * as meta from "./package.json";
 import { default as configure }  from '../../rollup.configurator';
 export default configure(meta, {
-  globals : {
-    'Buffer' : ['buffer', 'Buffer']
-  },
-  external : [ 'stream', 'buffer' ],
+  globals : { },
+  external : [ 'stream', 'process' ],
 });
--- packages/agen-stream/src/streamToPromise.js
+++ packages/agen-stream/src/streamToPromise.js
@@ -1,4 +1,42 @@
+
+// // First option:
 // import { finished } from 'stream';
 // import { promisify } from 'util';
 // export const streamToPromise = promisify(finished);
-module.exports.streamToPromise = require('stream-to-promise');
+
+// // Second option:
+// const eos = require('end-of-stream');
+// module.exports.streamToPromise = async function(stream) {
+//   return new Promise((resolve, reject) => eos(stream, (err, result) => {
+//     if (err) reject(err);
+//     else resolve(result);
+//   }))
+// }
+//
+
+// // Third option:
+export async function streamToPromise(stream) {
+  let list = []
+  function once(eventKey, action) {
+    function handler(...args) {
+      for (let i = 0; i < list.length; i++) list[i]();
+      list = [];
+      action(...args);
+    }
+    stream.on(eventKey, handler);
+    list.push(() => stream.removeListener(eventKey, handler));
+  }
+  return new Promise((resolve, reject) => {
+    try {
+      once('finish', resolve);
+      once('close', resolve);
+      once('end', resolve);
+      once('error', reject);
+      // ??
+      once('complete', resolve);
+      once('abort', reject);
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
