0294988380a8965fdef1009987ec7b3fa7bbed7d
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2020-11-10 02:51:38 +0100

    chore: add export for the GetterSetterBuilder class and the headerHandlers function

--- packages/agen-json/src/index.js
+++ packages/agen-json/src/index.js
@@ -3,3 +3,6 @@ export { jsonFromStrings } from './jsonFromStrings';
 export { jsonToArrays } from './jsonToArrays';
 export { jsonToGeojson } from './jsonToGeojson';
 export { jsonToStrings } from './jsonToStrings';
+
+export { GetterSetterBuilder } from './GetterSetterBuilder';
+export * from './headerHandlers';
